3951ed10854662f6e670f2ce7227c316cbe89687
emanuel-gold <96850688+emanuel-gold@users.noreply.github.com>
2025-10-01 23:37:59 -0400

    FEAT: massive color overhaul; adding neon glow effect on dark mode and new brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,14 @@ export default {
         }
       },
       colors: {
-        darkGradientEnd: "#060d23",
-        lightGradientStart: "#22d3ee1a",
+        "dark-gradient-end": "#060d23",
+        "light-gradient-start": "#DEF8FC",
         "brand-gold": "oklch(0.85 0.16 86.49)",
         "brand-fuchsia": "oklch(0.58 0.27 338.56)",
         "brand-burnt-orange": "#F87313",
         "brand-gradient-midpoint": "#e69d40",
+        "brand-dark-shadow-lch": "0.51 0.25 263.16",
+        "brand-dark-shadow": "oklch(var(--brand-dark-shadow-lch) / <alpha-value>)",
       },
     },
   },
